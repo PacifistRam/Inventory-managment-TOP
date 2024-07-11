c8d5963441c57d1c25b89c3f651bd66b5676f48e
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-07-11 22:21:16 +0530

    added all read operation routes with controllers

--- controllers/animeCategoryController.js
+++ controllers/animeCategoryController.js
@@ -1,42 +1,65 @@
 const AnimeCategory = require("../models/animeCategory");
+const Anime = require("../models/anime");
 const asyncHandler = require("express-async-handler");
 
-
-
 // display all Anime categories
-exports.anime_category_list = asyncHandler(async (req,res,next) => {
-    res.send("NOT Implemented: Anime category list");
+exports.anime_category_list = asyncHandler(async (req, res, next) => {
+  const allCategories = await AnimeCategory.find({}, "category_name")
+    .sort({ category_name: 1 })
+    .exec();
+
+  res.render("category_list", {
+    title: "Anime Categories",
+    anime_categories: allCategories,
+  });
 });
 
 // display detail page for specific anime category
-exports.anime_category_detail = asyncHandler(async (req,res,next) => {
-    res.send(`NOT IMPLEMENTED: Anime category detail: ${req.params.id}`);
-})
+exports.anime_category_detail = asyncHandler(async (req, res, next) => {
+  const [animeCategory, anime] = await Promise.all([
+    AnimeCategory.findById(req.params.id).exec(),
+    Anime.find({ anime_category: req.params.id }).exec(),
+  ]);
+
+    if (animeCategory === null) {
+        // no result
+        const err = new Error("Category not found ");
+        err.status = 404;
+        return next(err)
+    }
+    console.log(animeCategory)
+    res.render("Anime_category_detail", {
+        title: animeCategory.category_name,
+        anime_category: animeCategory,
+        animeFound:anime,
+    })
+});
 
 // display anime category create form
-exports.anime_category_create_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category create get");
-})
+exports.anime_category_create_get = (req, res, next) => {
+  res.render("category_form",{title: "Create Category"});
+};
 
 //  handle anime create post
-exports.anime_category_create_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category create post");
-})
+exports.anime_category_create_post = asyncHandler(async (req, res, next) => {
+  console.log(req.body)
+  res.send("NOT IMPLEMENTED: Anime category create post");
+});
 
 //  handle anime delete get
-exports.anime_category_delete_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category delete get");
-})
+exports.anime_category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime category delete get");
+});
 
 //  handle anime delete post
-exports.anime_category_delete_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category delete post");
-})
+exports.anime_category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime category delete post");
+});
 //  display anime update get form
-exports.anime_category_update_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category update get");
-})
+exports.anime_category_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime category update get");
+});
 //  handle anime update post
-exports.anime_category_update_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime category update post");
-})
+exports.anime_category_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime category update post");
+});
--- controllers/animeController.js
+++ controllers/animeController.js
@@ -2,54 +2,74 @@ const Anime = require("../models/anime");
 const AnimeCategory = require("../models/animeCategory");
 const asyncHandler = require("express-async-handler");
 
-exports.index = asyncHandler(async (req,res,next) => {
-    // get details of all anime's and categories present in this inventory
-    const [numAnimeCategory, numAnime] = await Promise.all([
-        AnimeCategory.countDocuments({}).exec(),
-        Anime.countDocuments({}).exec(),
-    ])
-    res.render("index",{
-        title: "Anime Catalog",
-        anime_category_count: numAnimeCategory,
-        anime_count: numAnime,
-
-    })
-})
+exports.index = asyncHandler(async (req, res, next) => {
+  // get details of all anime's and categories present in this inventory
+  const [numAnimeCategory, numAnime] = await Promise.all([
+    AnimeCategory.countDocuments({}).exec(),
+    Anime.countDocuments({}).exec(),
+  ]);
+  res.render("index", {
+    title: "Anime Catalog",
+    anime_category_count: numAnimeCategory,
+    anime_count: numAnime,
+  });
+});
 
 // display all Anime's
-exports.anime_list = asyncHandler(async (req,res,next) => {
-    res.send("NOT Implemented: Anime list");
+exports.anime_list = asyncHandler(async (req, res, next) => {
+  const allAnimes = await Anime.find(
+    {},
+    "anime_title anime_category rating status episodes"
+  )
+    .sort({ anime_title:1 })
+    .populate("anime_category")
+    .exec();
+  
+    res.render("anime_list",{ title:"Anime List", anime_list:allAnimes})
 });
 
 // display detail page for specific anime
-exports.anime_detail = asyncHandler(async (req,res,next) => {
-    res.send(`NOT IMPLEMENTED: Anime detail: ${req.params.id}`);
-})
+exports.anime_detail = asyncHandler(async (req, res, next) => {
+  const anime = await Anime.findById(req.params.id).populate("anime_category").exec()
+
+  if(anime === null) {
+    // no results
+    const err = new Error("Anime not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("anime_detail",{
+    title:anime.anime_title,
+    animeCategoryName: anime.anime_category.category_name,
+    animeDetails: anime,
+  })
+});
 
 // display anime create form
-exports.anime_create_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime create get");
-})
+exports.anime_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime create get");
+});
 
 //  handle anime create post
-exports.anime_create_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime create post");
-})
+exports.anime_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime create post");
+});
 
 //  handle anime delete get
-exports.anime_delete_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime delete get");
-})
+exports.anime_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime delete get");
+});
 
 //  handle anime delete post
-exports.anime_delete_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime delete post");
-})
+exports.anime_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime delete post");
+});
 //  display anime update get form
-exports.anime_update_get = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime update get");
-})
+exports.anime_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime update get");
+});
 //  handle anime update post
-exports.anime_update_post = asyncHandler(async (req,res,next) => {
-    res.send("NOT IMPLEMENTED: Anime update post");
-})
+exports.anime_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Anime update post");
+});
